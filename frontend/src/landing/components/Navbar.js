ddecb1ea0f24ccccd1cca8571390a0446b333b42
rohitkumar0511 <147647298+rohitkumar0511@users.noreply.github.com>
2026-03-09 16:30:10 +0000

    navbar change

--- frontend/src/landing/components/Navbar.js
+++ frontend/src/landing/components/Navbar.js
@@ -21,7 +21,7 @@ function Navbar() {
         <div className="logo-section">
           <img src={logo} alt="Board Logo" className="logo" />
           <h5>Board of Vocational and Skills Higher Secondary Education</h5>
-          <p>Ramtek, Nagpur (Maharashtra)</p>
+          <p>Bhopal, Madhya Pradesh</p>
         </div>
 
         <ul className="nav-menu">
