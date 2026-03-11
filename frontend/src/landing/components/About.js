1d8486bc35360afc3e5bf604393753d253737d00
rohitkumar0511 <147647298+rohitkumar0511@users.noreply.github.com>
2026-03-11 10:45:52 +0530

    about change

--- frontend/src/landing/components/About.js
+++ frontend/src/landing/components/About.js
@@ -10,14 +10,32 @@ function About() {
 
         <p>
           Board of Vocational and Skills Higher Secondary Education has been
-          established by an Act 1882. The board aims to extend support to
-          students of minority communities, weaker sections, and especially
-          promote girls' education.
+          established by anAct 1882. Vidarbha Board of Secondary Education is an
+          autonomous body that is established for the development of education
+          under the guidelines of National Education Policy 1986 and Programme
+          of Action 1992, Govt. of India. The prime objective of the board is to
+          extend help and all support to the students of Minority Community and
+          weaker sections of the Society with special emphasis on girls’
+          education. The mission of the board is not only to educate the
+          students of Minority Community but also educate all students of India
+          who have not been able to get an education for one or other reasons.
+        </p>
+        <p>
+          Board of Vocational and Skills Higher Secondary Education is running
+          8th Standard, Secondary and Senior Secondary level courses in English
+          and Hindi medium on the pattern of CBSE/NCERT for students of Minority
+          Community with special emphasis on girls education, rural youth,
+          working men and women, SC/ST, OBC and other disadvantaged persons who
+          could not continue their formal education for some reasons.
         </p>
 
         <p>
-          The board runs Secondary and Senior Secondary level courses in English
-          and Hindi medium following CBSE/NCERT pattern.
+          Our board has adopted the syllabus of NCERT, New Delhi. In
+          consideration of this many reputed institutes/Universities/Boards of
+          India and aboard has given positive responses by allowing our students
+          to take further admission in higher studies of their institutions by
+          extending recognition of the courses of Secondary and Senior Secondary
+          of Vidarbha Board of Secondary Education.
         </p>
 
         <a href="/contact">
