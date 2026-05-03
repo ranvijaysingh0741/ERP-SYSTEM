ee012c5f2b7b8ef0b32bddf310f9924afa41c256
sakshiupadhyay1 <sakshiupadhyay2005@gmail.com>
2026-05-03 14:37:28 +0530

    Update Fees.js

--- frontend/src/admin/pages/Fees.js
+++ frontend/src/admin/pages/Fees.js
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import AdminLayout from "../components/AdminLayout"; // ✅ ADD
 import "../styles/fees.css";
 
 const Fees = () => {
@@ -9,32 +8,32 @@ const Fees = () => {
   const navigate = useNavigate();
 
   return (
-    <AdminLayout>
-      <div className="fees-page">
+    <div className="fees-page">
 
-        <h2>Fees Module</h2>
+      <h2>Fees Module</h2>
 
-        <div className="session-select">
-          <label>Select Session:</label>
-          <select
-            value={session}
-            onChange={(e) => setSession(e.target.value)}
-          >
-            <option>2023-2024</option>
-            <option>2024-2025</option>
-            <option>2025-2026</option>
-          </select>
-        </div>
-
-        <button
-          className="primary-btn"
-          onClick={() => navigate(`/admin/fees/${session}`)} // ✅ FIX
+      <div className="session-select">
+        <label>Select Session:</label>
+        <select
+          value={session}
+          onChange={(e) => setSession(e.target.value)}
         >
-          Select Class
-        </button>
-
+          <option>2023-2024</option>
+          <option>2024-2025</option>
+          <option>2025-2026</option>
+        </select>
       </div>
-    </AdminLayout>
+
+      <button  style={{background:"#8b5e3c", padding:"20px"}}
+
+
+        className="primary-btn"
+        onClick={() => navigate(`/fees/${session}`)}
+      >
+        Select Class
+      </button>
+
+    </div>
   );
 };
 
