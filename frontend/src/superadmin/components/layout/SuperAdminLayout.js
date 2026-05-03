5e5a915bf705a562e88e29104dcd89e37fa7d7e9
sakshiupadhyay1 <sakshiupadhyay2005@gmail.com>
2026-05-03 18:53:16 +0530

    Update SuperAdminLayout.js

--- frontend/src/superadmin/components/layout/SuperAdminLayout.js
+++ frontend/src/superadmin/components/layout/SuperAdminLayout.js
@@ -1,26 +1,43 @@
 import React from "react";
-import { Outlet } from "react-router-dom";
+import { FaBars, FaBell, FaSearch, FaChevronDown } from "react-icons/fa";
 import SuperAdminSidebar from "../SuperAdminSidebar";
 import "../../styles/superadmin.css";
 
-const SuperAdminLayout = () => {
+const SuperAdminLayout = ({ children }) => {
   return (
     <div className="layout">
-
       <SuperAdminSidebar />
 
       <div className="main-content">
-
         <div className="topbar">
+          <div className="menu-box">
+            <FaBars />
+          </div>
+
           <h2>SUPER ADMIN PANEL</h2>
+
+          <div className="topbar-search">
+            <FaSearch />
+            <input type="text" placeholder="Search anything..." />
+          </div>
+
+          <div className="notification">
+            <FaBell />
+            <span>4</span>
+          </div>
+
+          <div className="admin-profile">
+            <div className="profile-circle">SA</div>
+            <div>
+              <h4>Super Admin</h4>
+              <p>Administrator</p>
+            </div>
+            <FaChevronDown />
+          </div>
         </div>
 
-        <div className="dashboard">
-          <Outlet />
-        </div>
-
+        <div className="page-content">{children}</div>
       </div>
-
     </div>
   );
 };
